3981ac218b6a555ade79b4e27f1c6e3c0c0e59a2
Marek Vesely <xvesely4@fi.muni.cz>
2023-03-29 09:00:14 +0200

    fixed some bugs

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,6 @@
-import sys 
-import os
 import yaml
 import flask
+
 
 app = flask.Flask(__name__)
 
@@ -12,8 +11,10 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
+
+
 class Person(object):
     def __init__(self, name):
         self.name = name
@@ -27,7 +28,7 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
+
     try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
@@ -37,23 +38,30 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    # deserializing data
+    deserialized_data = yaml.load(stream, Loader=yaml.Loader)
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
 
+
 if __name__ == '__main__':
     print("Vulnerabilities:")
-    print("1. Format string vulnerability: use string={person.__init__.__globals__[CONFIG][API_KEY]}")
-    print("2. Code injection vulnerability: use string=;print('Own code executed') #")
-    print("3. Yaml deserialization vulnerability: use string=file.yaml")
-    print("4. Use of assert statements vulnerability: run program with -O argument")
-    choice  = input("Select vulnerability: ")
-    if choice == "1": 
-        new_person = Person("Vickie")  
+    print("1. Format string vulnerability: use \
+          string={person.__init__.__globals__[CONFIG][API_KEY]}")
+    print("2. Code injection vulnerability: use \
+          string=;print('Own code executed') #")
+    print("3. Yaml deserialization vulnerability: use \
+          string=file.yaml")
+    print("4. Use of assert statements vulnerability: run \
+          program with -O argument")
+    choice = input("Select vulnerability: ")
+    if choice == "1":
+        new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
@@ -64,4 +72,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
